f3ef46ba3fb587db9b95a2e867ce3f9ecf727e30
dmschrein <darrelmschreiner@gmail.com>
2024-05-22 08:54:51 -0700

    added assets

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -1,45 +1,44 @@
-import { Schema, model, models } from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
 
 export interface IImage extends Document {
   _id: string;
   title: string;
   transformationType: string;
   publicId: string;
-  secureUrl: string; 
-  width?: number; 
-  height?: number; 
+  secureURL: string; 
+  width?: number;
+  height?: number;
   config?: object; 
   transformationUrl?: string; 
   aspectRatio?: string;
-  color?: string; 
+  color?: string;
   prompt?: string;
   author: {
     _id: string;
     firstName: string;
     lastName: string;
-  };
-  createdAt?: Date; 
-  updatedAt?: Date; 
+  }
+  createdAt?: Date;
+  updatedAt?: Date;
 }
-
 
 const ImageSchema = new Schema({
   title: { type: String, required: true },
   transformationType: { type: String, required: true },
   publicId: { type: String, required: true },
-  secureUrl: { type: URL, required: true },
+  secureURL: { type: String, required: true },
   width: { type: Number },
   height: { type: Number },
   config: { type: Object },
-  transformationUrl: { type: URL},
+  transformationUrl: { type: String },
   aspectRatio: { type: String },
   color: { type: String },
   prompt: { type: String },
   author: { type: Schema.Types.ObjectId, ref: 'User' },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
-})
+});
 
 const Image = models?.Image || model('Image', ImageSchema);
 
-export default Image; 
+export default Image;
